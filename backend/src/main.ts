37e9af1749064c8eaface05fff404f5bff61bb69
Kwangsoo Kim <gimgwangsu0@gmail.com>
2026-04-04 00:10:01 +0900

    feat: Swagger API 문서 기능 추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Talkertive API')
+    .setDescription('Talkertive API 문서입니다.')
+    .setVersion('1.0')
+    // @ApiBearerAuth('access-token') 데코레이터와 이름을 맞춰야 Swagger UI에서 인증이 연동됨
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'access-token',
+        description: 'Enter access token',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
