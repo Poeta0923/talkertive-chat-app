71d6e5ea9753db1e2b51777c21ec973d3ed002a3
Kwangsoo Kim <gimgwangsu0@gmail.com>
2026-04-04 13:06:52 +0900

    feat: prisma DB 연결 기능 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   // isGlobal: true — 모든 모듈에서 ConfigModule을 별도 import 없이 환경변수 사용 가능
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
